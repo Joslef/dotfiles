4bed1e237b5f46652a6255f94d7ca4200876bafe
Joerg <joerg.stoepel@googlemail.com>
2026-03-05 10:31:13 +0100

    Make changes to nvim to not deleting links when formatting .md files

---
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      -- always use the global markdownlint config so MD053 (link deletion) is disabled
+      -- regardless of the cwd when formatting
+      opts.formatters = opts.formatters or {}
+      opts.formatters["markdownlint-cli2"] = vim.tbl_deep_extend("force", opts.formatters["markdownlint-cli2"] or {}, {
+        prepend_args = { "--config", vim.fn.expand("~/.markdownlint-cli2.yaml") },
+      })
+    end,
+  },
+}

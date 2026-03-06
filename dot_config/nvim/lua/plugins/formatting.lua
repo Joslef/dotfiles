a3eccabb6bd6300da2e3ce9d9449ab0f5e4c7e0b
Joerg <joerg.stoepel@googlemail.com>
2026-03-06 21:18:37 +0100

    Change conform.nvim to use taplo with .toml files

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -8,6 +8,25 @@ return {
       opts.formatters["markdownlint-cli2"] = vim.tbl_deep_extend("force", opts.formatters["markdownlint-cli2"] or {}, {
         prepend_args = { "--config", vim.fn.expand("~/.markdownlint-cli2.yaml") },
       })
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft["toml"] = { "taplo" }
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        taplo = {
+          settings = {
+            taplo = {
+              -- prevent "this document is excluded" when no taplo.toml is present
+              config_file = {
+                enabled = false,
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }

9f566a163600d300a8e9dccd7fb3236ddbfcfde1
Joerg <joerg.stoepel@googlemail.com>
2026-03-16 08:52:18 +0100

    Make changes to update routine of calendar events in sketchybar

--- dot_config/sketchybar/items/widgets/next_event.lua
+++ dot_config/sketchybar/items/widgets/next_event.lua
@@ -15,10 +15,7 @@ local event = sbar.add("item", "widgets.next_event", {
   },
 })
 
-event:subscribe({ "routine", "forced" }, function()
-  local f = io.open("/tmp/sketchybar_next_event", "r")
-  local result = f and f:read("*a") or ""
-  if f then f:close() end
+local function update_display(result)
   result = result:gsub("[\n\r]", ""):gsub("^%s+", ""):gsub("%s+$", "")
   if result == "" then
     event:set({
@@ -31,6 +28,15 @@ event:subscribe({ "routine", "forced" }, function()
       icon = { color = settings.colors.magenta },
     })
   end
+end
+
+event:subscribe({ "routine", "forced" }, function()
+  sbar.exec(
+    "/opt/homebrew/bin/icalBuddy -n -li 1 -nc -nrd -eed -ea -df '' -tf '%H:%M' -iep 'datetime,title' -po 'datetime,title' -b '' -ps '/ /' eventsToday 2>/dev/null",
+    function(result)
+      update_display(result or "")
+    end
+  )
 end)
 
 event:subscribe("mouse.clicked", function()
